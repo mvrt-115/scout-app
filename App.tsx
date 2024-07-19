61d8108db70565a3f7098bac50f582a5dc477b32
Pranav Gupta <pranav04g@gmail.com>
2024-07-19 11:11:38 -0700

    update to sdk 51; will fix auth by migrating to firebase v9+

--- App.tsx
+++ App.tsx
@@ -1,17 +1,18 @@
 import { enableScreens } from "react-native-screens";
 import React, { useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
-import Home from "./pages/Home";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "react-native-screens/native-stack";
 import { ApplicationProvider } from "@ui-kitten/components";
 import * as eva from "@eva-design/eva";
+import Home from "./pages/Home";
 import QRScanner from "./pages/QRScanner";
 import Login from "./pages/Login";
 import Match from "./pages/Match";
 import PitScout from './pages/PitScout';
 import Comment from "./components/Comment";
-import { auth } from "./firebase";
+//import { auth } from "./firebase";
 
 console.warn = () => { };
 console.log = () => { };
@@ -22,41 +23,43 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <ApplicationProvider {...eva} theme={eva.light}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen
-            options={{ headerShown: false }}
-            name="Home"
-            component={Home}
-          />
-          <Stack.Screen
-            options={{ headerShown: false }}
-            name="QRScanner"
-            component={QRScanner}
-          />
-          <Stack.Screen
-            options={{ headerShown: false }}
-            name="Login"
-            component={Login}
-          />
-          <Stack.Screen
-            options={{ headerShown: false }}
-            name="Match"
-            component={Match}
-          />
-          <Stack.Screen
-            name="PitScout"
-            component={PitScout}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="Comment"
-            component={Comment}
-            options={{ headerShown: false }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </ApplicationProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen
+              options={{ headerShown: false }}
+              name="Home"
+              component={Home}
+            />
+            <Stack.Screen
+              options={{ headerShown: false }}
+              name="QRScanner"
+              component={QRScanner}
+            />
+            <Stack.Screen
+              options={{ headerShown: false }}
+              name="Login"
+              component={Login}
+            />
+            <Stack.Screen
+              options={{ headerShown: false }}
+              name="Match"
+              component={Match}
+            />
+            <Stack.Screen
+              name="PitScout"
+              component={PitScout}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Comment"
+              component={Comment}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ApplicationProvider>
+    </GestureHandlerRootView>
   );
 }

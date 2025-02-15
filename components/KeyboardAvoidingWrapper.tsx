290783e8ee1dc5090f9527f7ed8a1bb6df7baa93
Pranav Gupta <pranav04g@gmail.com>
2025-02-15 13:55:28 -0800

    save

--- components/KeyboardAvoidingWrapper.tsx
+++ components/KeyboardAvoidingWrapper.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'; 
 
 // keyboard avoiding view
 import { KeyboardAvoidingView, ScrollView, TouchableWithoutFeedback, Keyboard} from 'react-native';

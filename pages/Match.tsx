5fa7e2347b751bbcca9a7918fc3ce4a912e57907
Pranav Gupta <pranav04g@gmail.com>
2025-03-23 15:54:09 -0700

    more fields here we go again

--- pages/Match.tsx
+++ pages/Match.tsx
@@ -91,13 +91,14 @@ const Match: FC<MatchProps> = ({ route, navigation }) => {
             "Auton Coral Level 2 Missed: counter", "Auton Coral Level 3 Scored: counter", "Auton Coral Level 3 Missed: counter", 
             "Auton Coral Level 4 Scored: counter", "Auton Coral Level 4 Missed: counter", "Auton Algae Processor Scored: counter", 
             "Auton Algae Processor Missed: counter", "Auton Algae Net Scored: counter", "Auton Algae Net Missed: counter", 
-            "Mobility: boolean", "Starts on 1: boolean", "Starts on 2: boolean", "Starts on 3: boolean", "Starts on 4: boolean", "Starts on 5: boolean"]
+            "Mobility: boolean", "Starts on 1: boolean", "Starts on 2: boolean", "Starts on 3: boolean", "Starts on 4: boolean", 
+            "Starts on 5: boolean", "Auton Algae Removed Success: counter", "Auton Algae Removed Failed: counter"]
         setAutonFields(autonStuff.map((field: any) => getData(field)));
         const teleopStuff = ["Teleop Coral Level 1 Scored: counter", "Teleop Coral Level 1 Missed: counter", "Teleop Coral Level 2 Scored: counter", 
             "Teleop Coral Level 2 Missed: counter", "Teleop Coral Level 3 Scored: counter", "Teleop Coral Level 3 Missed: counter", 
             "Teleop Coral Level 4 Scored: counter", "Teleop Coral Level 4 Missed: counter", "Teleop Algae Processor Scored: counter", 
             "Teleop Algae Processor Missed: counter", "Teleop Algae Net Scored: counter", "Teleop Algae Net Missed: counter", 
-            "Played Defense: boolean", "Got Defended: boolean"]
+            "Played Defense: boolean", "Got Defended: boolean", "Teleop Algae Removed Success: counter", "Teleop Algae Removed Failed: counter"]
         setTeleopFields(teleopStuff.map((field: any) => getData(field)));
         // const endgameStuff = ["Did Climb: boolean", "Climb Time: timer", {"Climb Level": ["None", "Shallow", "Deep"]}, 
         //     "Recieved Auton RP: boolean", "Recieved Coral RP: boolean", "Recieved Coopertition RP: boolean", 

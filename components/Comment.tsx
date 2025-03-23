aab578e54ae22f6249c779a9df9d06b5c8d18c05
Pranav Gupta <pranav04g@gmail.com>
2025-03-22 23:16:37 -0700

    update camb to idbo

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -19,8 +19,8 @@ interface Team {
 
 const Comment: FC<CommentProps> = ({ navigation }) => {
 
-    const [regionals, setRegionals] = useState<string[]>(['camb']);
-    const [regional, setRegional] = useState<string>('camb');
+    const [regionals, setRegionals] = useState<string[]>(['idbo']);
+    const [regional, setRegional] = useState<string>('idbo');
     const [hasData, setHasData] = useState<boolean>(false);
     const [loading, setLoading] = useState<boolean>(true);
     const [teams, setTeams] = useState<Team[]>([{name: '', value: false}]);

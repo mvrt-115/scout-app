39293f91df76d1b885829d72f47c03edc87a3544
Pranav Gupta <pranav04g@gmail.com>
2025-09-28 17:49:06 -0700

    calgames update

--- components/PitScoutForm.tsx
+++ components/PitScoutForm.tsx
@@ -18,8 +18,8 @@ const PitScoutForm: FC<PitScoutProps> = ({ navigation }) => {
 
     const pitScoutFields = usePitScout((state) => state.pitScoutFields);
     const setPitScoutFields = usePitScout((state) => state.setPitScoutFields);
-    const [regionals, setRegionals] = useState<string[]>(['sunshow']);
-    const [regional, setRegional] = useState<string>('sunshow');
+    const [regionals, setRegionals] = useState<string[]>(['cacg']);
+    const [regional, setRegional] = useState<string>('cacg');
     const year = new Date().getFullYear();
     const [teamNum, setTeamNum] = useState<number>();
     const [hasData, setHasData] = useState<boolean>(false);
